958b33403e7b5197c5a89154eec5d0724900061e
Dawnflying <frankxiaofeilxf@gmail.com>
2023-02-24 05:45:34 +0800

    init

---
+++ src/main/java/org/jarvis/easyiot/EasyiotApplication.java
@@ -0,0 +1,13 @@
+package org.jarvis.easyiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyiotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyiotApplication.class, args);
+	}
+
+}
