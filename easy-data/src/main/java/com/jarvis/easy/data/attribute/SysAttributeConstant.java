e8e830c8e4cf48115cbcc0c0e1474e06e4994b27
Dawnflying <frankxiaofeilxf@gmail.com>
2023-02-27 19:49:23 +0800

    modified: 1. add SysAttributeConstant.

---
+++ easy-data/src/main/java/com/jarvis/easy/data/attribute/SysAttributeConstant.java
@@ -0,0 +1,8 @@
+package com.jarvis.easy.data.attribute;
+
+public class SysAttributeConstant {
+
+    public static final String SYS_PROTO_ID = "_sys_proto_id";
+
+    public static final String SYS_PROFILE_ID = "_sys_profile_id";
+}
